0386e18a63f5e9b2e1d9f61a3b67fc78d607e09b
Yev <yev.berezovsky@gmail.com>
2017-03-30 12:13:16 -0400

    Second Step changes

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -19,14 +19,28 @@ var AppComponent = (function () {
             id: 1,
             name: 'Windstorm'
         };
+        this.heroes = HEROES;
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n\t<h1>Hello {{title}}</h1>\n\t<h2>{{hero.name}} details!</h2>\n\t<div> <label>id: </label>{{hero.id}}</div>\n\t<div> \n\t<label>name: </label>\n\t<input [(ngModel)]=\"hero.name\" placeholder='name'>\n\t</div>\n\t",
+        template: "\n\t\t<h1>Hello {{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class = \"heroes\">\n\t\t<li *ngFor= \"let hero of heroes\">\n\t\t<span class = \"badge\">{{hero.id}} </span>{{hero.name}} \n\t\t</li>\n\t\t</ul>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<div> <label>id: </label>{{hero.id}}</div>\n\t\t<div> \n\t\t<label>name: </label>\n\t\t<input [(ngModel)]=\"hero.name\" placeholder='name'>\n\t\t</div>\n\t",
+        styles: ["\n\t\t.selected {\n\t\tbackground-color: #CFD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes {\n\t\tmargin: 0 0 2em 0;\n\t\tlist-style-type: none;\n\t\tpadding: 0;\n\t\twidth: 15em;\n\t\t}\n\t\t.heroes li {\n\t\tcursor: pointer;\n\t\tposition: relative;\n\t\tleft: 0;\n\t\tbackground-color: #EEE;\n\t\tmargin: .5em;\n\t\tpadding: .3em 0;\n\t\theight: 1.6em;\n\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\tbackground-color: #BBD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\tcolor: #607D8B;\n\t\tbackground-color: #DDD;\n\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\tposition: relative;\n\t\ttop: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\tdisplay: inline-block;\n\t\tfont-size: small;\n\t\tcolor: white;\n\t\tpadding: 0.8em 0.7em 0 0.7em;\n\t\tbackground-color: #607D8B;\n\t\tline-height: 1em;\n\t\tposition: relative;\n\t\tleft: -1px;\n\t\ttop: -4px;\n\t\theight: 1.8em;\n\t\tmargin-right: .8em;\n\t\tborder-radius: 4px 0 0 4px;\n  }"]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
