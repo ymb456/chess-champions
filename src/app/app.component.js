5f6b5c2753fc1edde2498a4200413653109d55bf
Yev <yev.berezovsky@gmail.com>
2017-04-04 15:19:38 -0400

    Added Service

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,14 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var hero_service_1 = require("./hero.service");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
         this.title = 'Tour of Heros';
-        this.heroes = HEROES;
     }
+    ;
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
+    };
+    ;
+    AppComponent.prototype.getHeroes = function () {
+        this.heroes = this.heroService.getHeroes();
+    };
+    ;
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
     };
     return AppComponent;
 }());
@@ -20,20 +33,11 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         template: "\n\t<h1>Hello {{title}}</h1>\n\t<h2>My Heroes</h2>\n\t<ul class = \"heroes\">\n\t   <li [class.selected]=\"hero === selectedHero\" \n\t   *ngFor= \"let hero of heroes\" (click)=\"onSelect(hero) \">\n\t   <span class = \"badge\">{{hero.id}} </span>{{hero.name}} \n\t   </li>\n\t</ul>\t\n\t<hero-detail [hero] = \"selectedHero\"></hero-detail>\n\t",
-        styles: ["\n\t\t.selected {\n\t\tbackground-color: #CFD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes {\n\t\tmargin: 0 0 2em 0;\n\t\tlist-style-type: none;\n\t\tpadding: 0;\n\t\twidth: 15em;\n\t\t}\n\t\t.heroes li {\n\t\tcursor: pointer;\n\t\tposition: relative;\n\t\tleft: 0;\n\t\tbackground-color: #EEE;\n\t\tmargin: .5em;\n\t\tpadding: .3em 0;\n\t\theight: 1.6em;\n\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\tbackground-color: #BBD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\tcolor: #607D8B;\n\t\tbackground-color: #DDD;\n\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\tposition: relative;\n\t\ttop: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\tdisplay: inline-block;\n\t\tfont-size: small;\n\t\tcolor: white;\n\t\tpadding: 0.8em 0.7em 0 0.7em;\n\t\tbackground-color: #607D8B;\n\t\tline-height: 1em;\n\t\tposition: relative;\n\t\tleft: -1px;\n\t\ttop: -4px;\n\t\theight: 1.8em;\n\t\tmargin-right: .8em;\n\t\tborder-radius: 4px 0 0 4px;\n  }"]
-    })
+        styles: ["\n\t\t.selected {\n\t\tbackground-color: #CFD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes {\n\t\tmargin: 0 0 2em 0;\n\t\tlist-style-type: none;\n\t\tpadding: 0;\n\t\twidth: 15em;\n\t\t}\n\t\t.heroes li {\n\t\tcursor: pointer;\n\t\tposition: relative;\n\t\tleft: 0;\n\t\tbackground-color: #EEE;\n\t\tmargin: .5em;\n\t\tpadding: .3em 0;\n\t\theight: 1.6em;\n\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\tbackground-color: #BBD8DC !important;\n\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\tcolor: #607D8B;\n\t\tbackground-color: #DDD;\n\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\tposition: relative;\n\t\ttop: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\tdisplay: inline-block;\n\t\tfont-size: small;\n\t\tcolor: white;\n\t\tpadding: 0.8em 0.7em 0 0.7em;\n\t\tbackground-color: #607D8B;\n\t\tline-height: 1em;\n\t\tposition: relative;\n\t\tleft: -1px;\n\t\ttop: -4px;\n\t\theight: 1.8em;\n\t\tmargin-right: .8em;\n\t\tborder-radius: 4px 0 0 4px;\n  }"],
+        providers: [hero_service_1.HeroService]
+    }),
+    __metadata("design:paramtypes", [hero_service_1.HeroService])
 ], AppComponent);
 exports.AppComponent = AppComponent;
-var HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+;
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service'
+
 
 
 @Component({
@@ -62,27 +64,25 @@ import { Hero } from './hero';
 		height: 1.8em;
 		margin-right: .8em;
 		border-radius: 4px 0 0 4px;
-  }`]
+  }`],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor (private heroService: HeroService) {
+
+	};
 	title = 'Tour of Heros';
 	selectedHero: Hero;
-	heroes = HEROES;
+	heroes: Hero[];
 	onSelect (hero: Hero): void {
 		this.selectedHero = hero;
+	};
+	getHeroes(): void  {
+		this.heroes = this.heroService.getHeroes();
+	};
+	ngOnInit (): void {
+		this.getHeroes();
 	}
-}
+};
 
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
